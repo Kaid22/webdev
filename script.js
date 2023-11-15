3193a56e3b781438b80e7ded70a9af8cf97b11fd
Kaid22 <avdickaid@gmail.com>
2023-11-15 15:35:42 +0100

    komita

--- script.js
+++ script.js
@@ -2,5 +2,5 @@ var back = document.getElementById("dogs");
 
 back.addEventListener("click", goSomewhere);
 function goSomewhere() {
-  window.location.href = "nekosbest Api/nekos.html";
+  window.location.href = "Dogs/dogs.html";
 }
